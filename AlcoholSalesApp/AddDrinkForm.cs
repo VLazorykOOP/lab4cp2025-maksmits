dec8c2efadd34cb3aa0345d2e0e203499e7936c0
Maksim Mitserskyi <m.maksim20051122@gmail.com>
2025-06-25 19:13:35 +0300

    git init

---
+++ AlcoholSalesApp/AddDrinkForm.cs
@@ -0,0 +1,217 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public partial class AddDrinkForm : Form
+{
+    private TextBox txtType;
+    private TextBox txtBrand;
+    private TextBox txtManufacturer;
+    private TextBox txtSupplier;
+    private TextBox txtPrice;
+    private DateTimePicker dtpExpiration;
+    private Button btnSave;
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private Label label4;
+    private Label label5;
+    private Label label6;
+    string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mmaks\source\repos\AlcoholSalesApp\AlcoholSales.mdf;Integrated Security=True";
+
+    public AddDrinkForm()
+    {
+        InitializeComponent();
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+            string query = "INSERT INTO Drinks (Type, Brand, Manufacturer, ExpirationDate, Supplier, Price) " +
+                           "VALUES (@Type, @Brand, @Manufacturer, @ExpirationDate, @Supplier, @Price)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Type", txtType.Text);
+            cmd.Parameters.AddWithValue("@Brand", txtBrand.Text);
+            cmd.Parameters.AddWithValue("@Manufacturer", txtManufacturer.Text);
+            cmd.Parameters.AddWithValue("@ExpirationDate", dtpExpiration.Value);
+            cmd.Parameters.AddWithValue("@Supplier", txtSupplier.Text);
+            cmd.Parameters.AddWithValue("@Price", decimal.Parse(txtPrice.Text));
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка збереження: " + ex.Message);
+            }
+
+
+            MessageBox.Show("Напій додано!");
+            this.Close();
+        }
+    }
+
+    private void InitializeComponent()
+    {
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.txtManufacturer = new System.Windows.Forms.TextBox();
+            this.txtSupplier = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.dtpExpiration = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(121, 31);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(100, 22);
+            this.txtType.TabIndex = 0;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(121, 68);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(100, 22);
+            this.txtBrand.TabIndex = 1;
+            // 
+            // txtManufacturer
+            // 
+            this.txtManufacturer.Location = new System.Drawing.Point(121, 106);
+            this.txtManufacturer.Name = "txtManufacturer";
+            this.txtManufacturer.Size = new System.Drawing.Size(100, 22);
+            this.txtManufacturer.TabIndex = 2;
+            // 
+            // txtSupplier
+            // 
+            this.txtSupplier.Location = new System.Drawing.Point(348, 31);
+            this.txtSupplier.Name = "txtSupplier";
+            this.txtSupplier.Size = new System.Drawing.Size(100, 22);
+            this.txtSupplier.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(348, 75);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // dtpExpiration
+            // 
+            this.dtpExpiration.Location = new System.Drawing.Point(248, 144);
+            this.dtpExpiration.Name = "dtpExpiration";
+            this.dtpExpiration.Size = new System.Drawing.Size(200, 22);
+            this.dtpExpiration.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(353, 192);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(95, 32);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Зберегти";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Тип";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Бренд";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Виробник";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Постачальник";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(239, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ціна";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(152, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Термін дії";
+            // 
+            // AddDrinkForm
+            // 
+            this.ClientSize = new System.Drawing.Size(469, 237);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtpExpiration);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtSupplier);
+            this.Controls.Add(this.txtManufacturer);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtType);
+            this.Name = "AddDrinkForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
+
---
+++ AlcoholSalesApp/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using AlcoholSalesApp;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class MainForm : Form
+{
+    private Button btnAddDrink;
+    private DataGridView dataGridView1;
+    string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\mmaks\\source\\repos\\AlcoholSalesApp\\AlcoholSales.mdf;Integrated Security=True";
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+        LoadDrinks();
+    }
+
+    private void LoadDrinks()
+    {
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Drinks", conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+
+    private void btnAddDrink_Click(object sender, EventArgs e)
+    {
+        AddDrinkForm form = new AddDrinkForm();
+        form.FormClosed += (s, args) => LoadDrinks(); // ← після закриття форми — оновлюємо таблицю
+        form.ShowDialog(); // НЕ .Show() !
+    }
+
+
+    private void InitializeComponent()
+    {
+            this.btnAddDrink = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddDrink
+            // 
+            this.btnAddDrink.Location = new System.Drawing.Point(447, 308);
+            this.btnAddDrink.Name = "btnAddDrink";
+            this.btnAddDrink.Size = new System.Drawing.Size(111, 39);
+            this.btnAddDrink.TabIndex = 3;
+            this.btnAddDrink.Text = "Додати";
+            this.btnAddDrink.UseVisualStyleBackColor = true;
+            this.btnAddDrink.Click += new System.EventHandler(this.btnAddDrink_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 290);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(570, 356);
+            this.Controls.Add(this.btnAddDrink);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+    }
+}
+
